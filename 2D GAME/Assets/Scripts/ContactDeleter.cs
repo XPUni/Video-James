eccc24606397bcbd0c82b94c8384432940d1c515
gnome-sketches <90193643+gnome-sketches@users.noreply.github.com>
2024-05-05 16:22:39 +1000

    Updated Ears Map
    - Added doors
    - Added ears
    - Added Earpods
    - Unable to pick up right ear without ear pods (correct!)
    - Still unable to pick up right ear after finding ear pods

--- 2D GAME/Assets/Scripts/ContactDeleter.cs
+++ 2D GAME/Assets/Scripts/ContactDeleter.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ContactDeleter : MonoBehaviour
-{
+{  
+    private bool hasEarPod;
     private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        hasEarPod = false;
     }
 
     // Update is called once per frame
@@ -27,6 +29,14 @@ public class ContactDeleter : MonoBehaviour
     {
         if (collision.gameObject.tag == "PlayerHand")
         {
+            if(gameObject.tag == "earPod"){
+                hasEarPod = true;
+                Debug.Log(hasEarPod);
+            }
+            if(gameObject.tag == "SecondEar" && !hasEarPod){  
+                Debug.Log("You don't have the second earpod.");
+                return;
+            }
             //player.GetComponent<PlayerController>().key1 = true;    
             Destroy(gameObject);
         }
