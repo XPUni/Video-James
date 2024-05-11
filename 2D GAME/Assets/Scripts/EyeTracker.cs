c730e6c904945cc0cd8941e0b342fc0f6ffefafa
Casey Bolton <caseylbolton@gmail.com>
2024-05-11 13:19:26 +1000

    eye level commit
    also fixed some mouth level bugs

--- 2D GAME/Assets/Scripts/EyeTracker.cs
+++ 2D GAME/Assets/Scripts/EyeTracker.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
-
+using UnityEngine.SceneManagement;
 
 public class EyeTracker : MonoBehaviour
 {
     PostProcessVolume m_Volume;
     ColorGrading m_Grayscale;
     private int eyes;
+    private bool triggerThisFrame = false;
     void Start()
         {
             eyes = 0;
@@ -20,12 +21,21 @@ public class EyeTracker : MonoBehaviour
             m_Volume = PostProcessManager.instance.QuickVolume(gameObject.layer, 100f, m_Grayscale);
         }
 
+    void Update() {
+        triggerThisFrame = false;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision) {
+        if (triggerThisFrame) { return; }
+        triggerThisFrame = true;
         if (collision.gameObject.tag == "Eye") {
             eyes += 1;
             Destroy(collision.gameObject);
             if (eyes==1) {
                 m_Grayscale.enabled.Override(false);
+            }
+            if (eyes==2) {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
             }
         }
     }
