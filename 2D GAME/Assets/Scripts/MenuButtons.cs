6b0b6beab5d1c47c487e32114bbf11c7da427434
cassandras12 <cassandra.shearwood@student.uts.edu.au>
2024-05-12 22:48:55 +1000

    fixing mouth map and ui, and menu

--- 2D GAME/Assets/Scripts/MenuButtons.cs
+++ 2D GAME/Assets/Scripts/MenuButtons.cs
@@ -8,7 +8,7 @@ public class MenuButtons : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene("Sample Scene");
+        SceneManager.LoadScene("HandCutscene");
     }
 
     public void QuitGame()
@@ -18,7 +18,7 @@ public class MenuButtons : MonoBehaviour
 
     public void Level1()
     {
-        SceneManager.LoadScene("Sample Scene");
+        SceneManager.LoadScene("HandCutscene");
     }
 
     public void Level2()
